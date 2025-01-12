0a19478a74c7b3f65df7235ed50587a99d0a2996
ameeenamzz <shoukethameen@gmail.com>
2025-01-12 11:29:22 +0530

    Add pattern8.c to print a complex star pattern

---
+++ pattern8.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main(){
+    int n=5;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            printf(" \t");
+        }
+        for(int j=1;j<=i;j++){
+            printf("*\t");
+        }
+        for(int j=0;j<=i;j++){
+            printf("*\t");
+        }
+        for(int j=i;j<n-2;j++){
+            printf(" \t");
+        }
+        for(int j=i;j<n-1;j++){
+            printf(" \t");
+        }
+         for(int j=1;j<=i;j++){
+            printf("*\t");
+        }
+        for(int j=0;j<=i;j++){
+            printf("*\t");
+        }
+        printf("\n");
+
+    }
+}
