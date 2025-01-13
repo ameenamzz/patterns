f3f8f6aa48f58c7657778cba2a139289f2752129
ameeenamzz <shoukethameen@gmail.com>
2025-01-13 17:34:27 +0530

    Add pattern11.c to print Z pattern of asterisks

---
+++ pattern11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    int n=5;
+    for(int i=0;i<n;i++){
+        for (int j = 0; j<n; j++)
+        {
+            if(i==0||i==n-1||i+j+1==n){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
